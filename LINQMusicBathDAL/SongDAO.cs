9aa271ff3e1a0245e0722f10d6e455b501435117
Osman A <osman.alper@yahoo.com>
2016-11-16 00:15:12 +0200

    Add files via upload

---
+++ LINQMusicBathDAL/SongDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQMusicBathBDO;
+
+namespace LINQMusicBathDAL
+{
+    public class SongDAO
+    {
+        public SongBDO GetSong(int id)
+        {
+            SongBDO songBDO = null;
+            using (var MBEntities = new MusicBathEntities())
+            {
+                Song product = (from s in MBEntities.Songs
+                                   where s.SongID == id
+                                   select s).FirstOrDefault();
+                if (product != null)
+                    songBDO = new SongBDO()
+                    {
+                        SongID = product.SongID,
+                        SongName = product.SongName,
+                    };
+            }
+            return songBDO;
+        }
+    }
+}
