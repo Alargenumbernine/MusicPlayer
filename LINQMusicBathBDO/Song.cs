59c6494c2c289e4fe9ae5b4bec290331bc37383f
Osman A <osman.alper@yahoo.com>
2016-11-16 00:15:34 +0200

    Add files via upload

---
+++ LINQMusicBathBDO/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQMusicBathBDO
+{
+    public class SongBDO
+    {
+        public int SongID { get; set; }
+        public string SongName { get; set; }
+    }
+}
